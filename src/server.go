0f9a3e34990de0354a74174eab167896c650240e
AliusDieMorietur <ikamqwerty@gmail.com>
2021-09-07 14:26:35 +0300

    First lab implementation

---
+++ src/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func serve(w http.ResponseWriter, r *http.Request) {
+	log.Println("url", r.URL.Path);
+	f := END_POINTS[r.URL.Path]
+	respose := "Not found"
+	if (f != nil) {
+		respose = f()
+	}
+	fmt.Fprintf(w, respose)
+}
+
+func main() {
+	port := getPort()
+	http.HandleFunc("/", serve)
+	log.Printf("Listening at http://localhost:%s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s",port), nil))
+}
---
+++ src/utils.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"time"
+)
+
+func getPort() string {
+	port := os.Getenv("PORT") 
+	if (port == "") {
+		port = "8765"
+	}
+	return port
+}
+
+func getTimeJSON() string {
+	log.Println("time.Now()", time.Now())
+	time, err := json.Marshal(map[string]string{"time": time.Now().Format(time.RFC1123)})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return string(time)
+}
