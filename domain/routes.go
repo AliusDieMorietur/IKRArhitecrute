931b5c121bd358312c3d689560c1e7e46102f00b
gidra5 <hranitel.52@gmail.com>
2021-09-11 23:16:38 +0300

    Move '/' handler to a separate func

--- domain/routes.go
+++ domain/routes.go
@@ -6,15 +6,16 @@ import (
 	"time"
 )
 
-var ROUTES = map[string]func() string{
-	"/":     func() string { return "/ - Help\n/time - Show current time" },
-	"/time": getTimeJSON,
-}
-
 type Package struct {
 	Time string `json:"time"`
 }
 
+var ROUTES = map[string]func() string{
+	"/":     rootHandler,
+	"/time": getTimeJSON,
+}
+
+func rootHandler() string { return "/ - Help\n/time - Show current time" }
 func getTimeJSON() string {
 	time, err := json.Marshal(Package{Time: time.Now().Format(time.RFC1123)})
 	if err != nil {
